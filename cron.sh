59aa530cd24e5f49b96065ea568dcaae33bef38b
gnd <gnd@itchybit.org>
2020-03-29 22:13:35 +0200

    adding some automated data retrieval

---
+++ cron.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# download new data
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -O data/confirmed.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -O data/deaths.csv
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv -O data/recovered.csv
+
+# now turn it into JS arrays
+./process_csv.py
---
+++ process_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import re
+
+def process_csv(name):
+    f = file('data/%s.csv' % name,'r')
+    lines = f.readlines()
+    f.close()
+
+    out = ""
+    for line in lines[1:]:
+        p = re.compile('(.*)\"(.*)(,)([^\"]*)\"')
+        line = p.sub(r'\1\2\4', line)
+        arr = line.split(',')
+        index = 0
+        vals = ""
+        for key in lines[0].split(',')[4:]:
+            vals += "'%s': %s, " % (key.strip(), arr[4+index].strip())
+            index += 1
+        if (arr[0] != ""):
+            out += "'%s, %s': { %s },\n" % (arr[1].replace("'","\\'"), arr[0], vals.strip(', '))
+        else:
+            out += "'%s': { %s },\n" % (arr[1].replace("'","\\'"), vals.strip(', '))
+
+    f = file('data/%s.js' % name, 'w')
+    if (name == 'confirmed'):
+        f.write("global_data = {};\n")
+    f.write("global_data['%s'] = {%s};" % (name, out))
+    f.close()
+
+process_csv('confirmed')
+process_csv('recovered')
+process_csv('deaths')
