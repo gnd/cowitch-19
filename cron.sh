cbb8d0f1157e9694d4432345c8e1127a301d0b55
gnd <gnd@itchybit.org>
2020-09-19 01:57:34 +0200

    cron script update

--- cron.sh
+++ cron.sh
@@ -6,6 +6,8 @@ cd $DIR
 wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -O data/confirmed.csv
 wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -O data/deaths.csv
 wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv -O data/recovered.csv
+wget https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.csv -O data/nakazeni-vyleceni-umrti-testy.csv
 
 # now turn it into JS arrays
 $DIR/process_csv.py
+$DIR/process_csv_cz.py
