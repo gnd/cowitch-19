71076e0652ce145fd2419cf5cd5f84e87604045d
gnd <gnd@itchybit.org>
2021-08-06 20:01:51 +0200

    js generator for cretan cities

---
+++ process_crete.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def process_csv(name):
+    f = open('data/eody_{}.txt'.format(name),'r')
+    lines = f.readlines()
+    f.close()
+
+    confirmed = ''
+    avg7 = ''
+    hundred_k = ''
+
+    for line in lines:
+        # example of one row in data:
+        # Datum confirmed avg7 100k
+        # 1/22/21 100 100 100
+        arr = line.split()
+        datum = arr[0]
+        confirmed += "'{}': {}, ".format(datum, arr[1])
+        avg7 += "'{}': {}, ".format(datum, arr[2])
+        hundred_k += "'{}': {}, ".format(datum, arr[3])
+
+    # Write the data into files - confirmed
+    f = open('data/eody_crete.js', 'a')
+    f.write("crete_data['confirmed']['{}'] = {{{}}};\n".format(name, confirmed.strip(', ')))
+    f.close()
+    # Write the data into files - avg7
+    f = open('data/eody_crete.js', 'a')
+    f.write("crete_data['avg7']['{}'] = {{{}}};\n".format(name, avg7.strip(', ')))
+    f.close()
+    # Write the data into files - 100k
+    f = open('data/eody_crete.js', 'a')
+    f.write("crete_data['hundred_k']['{}'] = {{{}}};\n".format(name, hundred_k.strip(', ')))
+    f.close()
+
+# Run processing
+# clean data file first
+f = open('data/eody_crete.js', 'w')
+f.write("crete_data = {};\n")
+f.close()
+
+# process data from OCR'd pdfs
+process_csv('chania')
+process_csv('heraklion')
+process_csv('lasithi')
+process_csv('rethymno')
+
