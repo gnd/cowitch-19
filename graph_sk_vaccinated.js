647319f2c9d97fa1dcb6eb173abdbc5a936e1e2f
gnd <gnd@itchybit.org>
2021-10-27 17:50:39 +0200

    extending time range

--- graph_sk_vaccinated.js
+++ graph_sk_vaccinated.js
@@ -42,7 +42,7 @@ vaccinated_dataset.push( {
 window.vaccinated_chart = new Chart(vaccinated_chart_sk, {
     type: 'line',
     data: {
-        labels: gen_days(4, 0, 190),
+        labels: gen_days(4, 0, 370),
         datasets: vaccinated_dataset,
     },
     options: {
--- graph_sk_vaccinated_daily_details.js
+++ graph_sk_vaccinated_daily_details.js
@@ -30,7 +30,7 @@ vaccinated_dataset.push( {
 window.vaccinated_chart = new Chart(vaccinated_chart_sk_det_daily, {
     type: 'line',
     data: {
-        labels: gen_days(4, 0, 190),
+        labels: gen_days(4, 0, 370),
         datasets: vaccinated_dataset,
     },
     options: {
